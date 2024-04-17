8867107c645972801753b8d1d5ae62f7a2da2bda
ghostnetrn <ghostnetrn@gmail.com>
2024-04-17 22:59:03 +0000

    increase profit

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const { Telegraf, Markup } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const SYMBOL = process.env.SYMBOL;
-const PROFIT = parseFloat(process.env.PROFIT);
+let PROFIT = parseFloat(process.env.PROFIT);
 const BUY_QTY = parseFloat(process.env.BUY_QTY);
+const PROFIT_INCREASE_PERCENT = parseFloat(process.env.PROFIT_INCREASE_PERCENT);
+const PROFIT_DECREASE_PERCENT = parseFloat(process.env.PROFIT_DECREASE_PERCENT);
 
 bot.telegram.sendMessage(process.env.CHAT_ID, `Iniciando monitoramento da moeda ${SYMBOL}`);
 
@@ -18,6 +20,7 @@ const ws = new WebSocket(
 //não mexa nestas variáveis
 let quantity = 0;
 let buyPrice = 0;
+let targetSell = 0;
 let sendMessage = false;
 let message = null ?? `Aguardando lançamento da moeda ${SYMBOL}`;
 
@@ -32,13 +35,103 @@ ws.on("error", async (err) => {
   process.exit(1);
 });
 
+// ws.onmessage = async (event) => {
+//   try {
+//     const obj = JSON.parse(event.data);
+//     console.clear();
+//     let targetPrice = (buyPrice * PROFIT).toFixed(2);
+//     const notional = `${buyPrice * quantity}`;
+//     let percentual = ((PROFIT - 1) * 100).toFixed() + "%";
+
+//     console.log(`Symbol: ${obj.s}`);
+//     console.log(`Best ask: ${obj.a}`);
+//     console.log(`Best bid: ${obj.b}`);
+//     console.log(`Buy Price: ${buyPrice}`);
+//     console.log(`Profit: ${percentual}`);
+//     console.log(`Qty: ${quantity}`);
+//     console.log(`Notional: ${notional}`);
+//     console.log(`Target Price: ${targetPrice} ${percentual}`);
+
+//     message = `
+// *Symbol*: ${obj.s}
+// *Best ask*: ${obj.a}
+// *Best bid*: ${obj.b}
+
+// *Buy Price*: ${buyPrice}
+// *PROFIT*: ${percentual}
+// *Qty*: ${quantity}
+// *Notional*: ${buyPrice * quantity}
+
+// *Target Price*: ${buyPrice * PROFIT} 
+// `;
+
+//     if (sendMessage) {
+//       bot.telegram.sendMessage(process.env.CHAT_ID, message, {
+//         parse_mode: "Markdown",
+//       });
+//       sendMessage = false;
+//     }
+
+//     if (quantity === 0) {
+//       quantity = -1;
+//       const order = await api.buy(SYMBOL, BUY_QTY);
+//       sendMessage = true;
+
+//       if (order.status !== "FILLED") {
+//         console.log(order);
+//         await bot.telegram.sendMessage(process.env.CHAT_ID, order);
+//         await bot.telegram.sendMessage(
+//           process.env.CHAT_ID,
+//           "O bot foi parado! Verifique"
+//         );
+//         process.exit(1);
+//       }
+
+//       quantity = parseFloat(order.executedQty);
+//       buyPrice = parseFloat(order.fills[0].price);
+//       return;
+//     } else if (quantity > 0 && parseFloat(obj.b) > buyPrice * PROFIT) {
+//       const order = await api.sell(SYMBOL, quantity);
+//       if (order.status !== "FILLED") {
+//         console.log(order);
+//         bot.telegram.sendMessage(process.env.CHAT_ID, order);
+//       } else {
+//         const soldPrice = parseFloat(order.fills[0].price).toFixed(2);
+//         const soldDate = new Date().toLocaleString("pt-BR");
+//         console.log(`Sold at ${soldDate} by USD ${soldPrice}`);
+
+//         await bot.telegram.sendMessage(
+//           process.env.CHAT_ID,
+//           `Vendido em ${soldDate} por ${order.fills[0].price}`
+//         );
+//         await bot.telegram.sendMessage(
+//           process.env.CHAT_ID,
+//           "*O bot foi parado! Verifique*",
+//           { parse_mode: "Markdown" }
+//         );
+//         process.exit(1);
+//       }
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     await bot.telegram.sendMessage(process.env.CHAT_ID, JSON.stringify(err));
+//     await bot.telegram.sendMessage(
+//       process.env.CHAT_ID,
+//       "O bot foi parado! Verifique"
+//     );
+//     process.exit(1);
+//   }
+// };
+
+// Telegram
+
 ws.onmessage = async (event) => {
   try {
     const obj = JSON.parse(event.data);
     console.clear();
-    const targetPrice = (buyPrice * PROFIT).toFixed(2);
+    let targetPrice = (buyPrice * PROFIT).toFixed(2);
     const notional = `${buyPrice * quantity}`;
-    const percentual = ((PROFIT - 1) * 100).toFixed() + "%";
+    let percentual = ((PROFIT - 1) * 100).toFixed() + "%";
 
     console.log(`Symbol: ${obj.s}`);
     console.log(`Best ask: ${obj.a}`);
@@ -47,7 +140,7 @@ ws.onmessage = async (event) => {
     console.log(`Profit: ${percentual}`);
     console.log(`Qty: ${quantity}`);
     console.log(`Notional: ${notional}`);
-    console.log(`Target Price: ${targetPrice} ${percentual}`);
+    console.log(`Target Price: ${targetPrice}`);
 
     message = `
 *Symbol*: ${obj.s}
@@ -57,9 +150,9 @@ ws.onmessage = async (event) => {
 *Buy Price*: ${buyPrice}
 *PROFIT*: ${percentual}
 *Qty*: ${quantity}
-*Notional*: ${buyPrice * quantity}
+*Notional*: ${notional}
 
-*Target Price*: ${buyPrice * PROFIT} 
+*Target Price*: ${targetPrice}
 `;
 
     if (sendMessage) {
@@ -86,27 +179,38 @@ ws.onmessage = async (event) => {
 
       quantity = parseFloat(order.executedQty);
       buyPrice = parseFloat(order.fills[0].price);
-      return;
-    } else if (quantity > 0 && parseFloat(obj.b) > buyPrice * PROFIT) {
-      const order = await api.sell(SYMBOL, quantity);
-      if (order.status !== "FILLED") {
-        console.log(order);
-        bot.telegram.sendMessage(process.env.CHAT_ID, order);
-      } else {
-        const soldPrice = parseFloat(order.fills[0].price).toFixed(2);
-        const soldDate = new Date().toLocaleString("pt-BR");
-        console.log(`Sold at ${soldDate} by USD ${soldPrice}`);
+      targetSell = buyPrice * PROFIT;
 
-        await bot.telegram.sendMessage(
-          process.env.CHAT_ID,
-          `Vendido em ${soldDate} por ${order.fills[0].price}`
-        );
-        await bot.telegram.sendMessage(
-          process.env.CHAT_ID,
-          "*O bot foi parado! Verifique*",
-          { parse_mode: "Markdown" }
-        );
-        process.exit(1);
+      return;
+    } else {
+      if (parseFloat(obj.b) > targetSell) {
+        // Se o preço atingir a meta de lucro
+        PROFIT *= PROFIT_INCREASE_PERCENT;
+        percentual = ((PROFIT - 1) * 100).toFixed() + "%"; // Recalcula o percentual de lucro
+        targetSell = buyPrice * PROFIT;
+        targetPrice = (targetSell).toFixed(2);
+      } else if (parseFloat(obj.b) < targetSell - (targetSell * PROFIT_DECREASE_PERCENT))  {
+        // Se o preço cair abaixo do lucro desejado
+        const order = await api.sell(SYMBOL, quantity);
+        if (order.status !== "FILLED") {
+          console.log(order);
+          bot.telegram.sendMessage(process.env.CHAT_ID, order);
+        } else {
+          const soldPrice = parseFloat(order.fills[0].price).toFixed(2);
+          const soldDate = new Date().toLocaleString("pt-BR");
+          console.log(`Sold at ${soldDate} by USD ${soldPrice}`);
+
+          await bot.telegram.sendMessage(
+            process.env.CHAT_ID,
+            `Vendido em ${soldDate} por ${order.fills[0].price}`
+          );
+          await bot.telegram.sendMessage(
+            process.env.CHAT_ID,
+            "*O bot foi parado! Verifique*",
+            { parse_mode: "Markdown" }
+          );
+          process.exit(1);
+        }
       }
     }
   } catch (err) {
@@ -120,7 +224,6 @@ ws.onmessage = async (event) => {
   }
 };
 
-// Telegram
 bot.start((ctx) =>
   ctx.reply("Bot para lançamento de criptomoedas na Binance", keyboard)
 );
